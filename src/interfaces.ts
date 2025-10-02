da5419d4f9f6a2b2134b18fbceaa5789ab8ef04a
Mara <mara-li@outlook.fr>
2025-10-02 23:21:49 +0200

    feat: add settings and create interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,51 @@
 export interface Settings {
-	mySetting: string;
+	/**
+	 * To get the properties names, they are two modes:
+	 * - Simple key (aka one properties to get the entire location): "123 Main St, Springfield, IL"
+	 * - Template mode (aka multiple properties to get the entire location): "{address}, {city}, {state}"
+	 */
+	inputKeys: {
+		mode: "simple" | "template";
+		simpleKey: string;
+		template: string;
+		/**
+		 * If true, the `.` in the template will be replaced by the `inputKeys.template` value.
+		 */
+		object: boolean;
+	},
+	outputFormat : {
+		/**
+		 * If true, the `.` in the outputFormat will be replaced by the `outputFormat.keyName` value.
+		 */
+		object: boolean;
+		/**
+		 * If "simple", will use two keys: `latitude` and `longitude`.
+		 * If multiple, allow to use one keys for the coordinate, in the form of a template: `{latitude}, {longitude}`.
+		 */
+		mode: "simple" | "multiple";
+		simpleKey: {
+			latitude: string;
+			longitude: string;
+		};
+		multipleKey: string;
+		
+	}
 }
 
 export const DEFAULT_SETTINGS: Settings = {
-	mySetting: "default",
+	inputKeys: {
+		mode: "simple",
+		simpleKey: "address",
+		object: false,
+		template: "{address}, {city}, {state}"
+	},
+	outputFormat: {
+		object: false,
+		mode: "simple",
+		simpleKey: {
+			latitude: "latitude",
+			longitude: "longitude"
+		},
+		multipleKey: "{latitude}, {longitude}"
+	}
 };
--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,8 @@
-import { type App, PluginSettingTab, Setting } from "obsidian";
+import {type App, Component, MarkdownRenderer, PluginSettingTab, Setting} from "obsidian";
 import type LocationToCoordinate from "./main";
-
+import "./i18n/obsidian-i18n";
+import dedent from "dedent";
+import i18next from "i18next";
 export class SettingTab extends PluginSettingTab {
 	plugin: LocationToCoordinate;
 
@@ -9,22 +11,48 @@ export class SettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
-	display(): void {
+	async display(): Promise<void> {
 		const { containerEl } = this;
 
 		containerEl.empty();
+		containerEl.addClass("loc-to-coord-settings");
 
 		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
+			.setNames("inputKeys.header")
+			.setHeading()
+		
+		const t= new Setting(containerEl)
+			.setNames("inputKeys.mode.name")
+			.setClass("mode")
+			.addDropdown((d) =>
+				d
+					.addOption("simple", i18next.t("inputKeys.mode.options.simple"))
+					.addOption("template", i18next.t("inputKeys.mode.options.template"))
+					.setValue(this.plugin.settings.inputKeys.mode)
 					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
+						this.plugin.settings.inputKeys.mode = value as "simple" | "template";
 						await this.plugin.saveSettings();
+						await this.display();
 					})
-			);
+			)
+		
+		const component = new Component()
+		await MarkdownRenderer.render(this.app, dedent(`${i18next.t("inputKeys.mode.desc")}`), t.descEl, "", component)
+		component.onunload();
+		
+		if (this.plugin.settings.inputKeys.mode === "simple") {
+			new Setting(containerEl)
+				.setNames("inputKeys.simpleKey.name")
+				.addText(
+					(text) =>
+						text
+							.setValue(this.plugin.settings.inputKeys.simpleKey)
+							.setPlaceholders("inputKeys.simpleKey.placeholder")
+							.onChange(async (value) => {
+								this.plugin.settings.inputKeys.simpleKey = value;
+								await this.plugin.saveSettings();
+							})
+				)
+		}
 	}
 }
